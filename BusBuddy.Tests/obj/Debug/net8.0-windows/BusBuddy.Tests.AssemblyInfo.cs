4df036b4c2b093db22b19f0c8cc8f21ff96fe920
Steve McKitrick <steve.mckitrick@example.com>
2025-06-09 05:44:15 -0600

    Fix GitHub Actions workflow YAML syntax and improve UI test handling

--- BusBuddy.Tests/obj/Debug/net8.0-windows/BusBuddy.Tests.AssemblyInfo.cs
+++ BusBuddy.Tests/obj/Debug/net8.0-windows/BusBuddy.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusBuddy.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e3ce201721e91eca1b0feb881e43b083a5b1ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d489bb701763154c58d10a2fee2b2c9e6b93e527")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusBuddy.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusBuddy.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
